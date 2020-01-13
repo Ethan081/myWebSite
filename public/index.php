af8cc824d8eb068e3988a7b75d5fc01cecfa2eea
zemac <etienne.ziegelmeyer@lapiscine.pro>
2020-01-13 11:13:21 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    <title>Etienne Ziegelmeyer</title>
+    <script src="https://kit.fontawesome.com/70ece4c924.js" crossorigin="anonymous"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+              <img src="assets/images/Logo-EZ.png" width="50" height="50" class="d-inline-block align-top" alt="">
+            </a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Contact</button>
+                </li>
+            </ul>
+          </nav>
+    </header>
+    <main>
+        <section class="home_page">
+            <div class="name_tag_line">
+                <h1>Developpeur Full Stack</h1>
+                <h2>Etienne ZIEGELMEYER</h2>
+                <p>....</p>
+            </div>    
+        </section>
+        <section class="presentation">
+            <div class="pres_message">
+                <h1>Bonjour je suis Etienne</h1>
+                <p>Je suis developpeur web Junior.
+                    J'ai fini ma formation de depeloppeur web cette annee...
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="competencies">
+                <div class="box">
+                    <div class="column_front">
+                    <div class="image">
+                        <img src="..." alt="..." class="img-thumbnail">
+                    </div>
+                        <h1>Front_End</h1>
+                        <p>bsdhdsjhds</p>
+                        <p>title</p>
+                        <ul>
+                            <li>php</li>
+                            <li>java</li>
+                            <li>python</li>
+                        </ul>
+                    </div>
+                    <div class="column_back">
+                        <div class="image">
+                            <img src="..." alt="..." class="img-thumbnail">
+                        </div>
+                        <h1>Back_End</h1>
+                        <p>bsdhdsjhds</p>
+                        <p>title</p>
+                        <ul>
+                            <li>php</li>
+                            <li>java</li>
+                            <li>python</li>
+                        </ul>
+                    </div> 
+                </div>
+            </div>
+        </section> 
+        <section class="recent_work">
+            <div class="project_title">
+                <h1>My Recent Work</h1>
+                <p>Here are a few recent design projects</p>
+            </div>
+            <div class="projects">
+                <div class="projet_box">
+                    <p>Box01</p>
+                </div>
+                <div class="projet_box">
+                <p>Box02</p>
+                </div>
+            </div>
+        </section>   
+    </main>
+    <footer>
+        <section class="footer">
+            <article class="logo">
+                <a href="#">
+                <img src="assets/images/Logo-EZ.png" width="50" height="50" class="d-inline-block align-top" alt="">
+                </a>
+            </article>
+            <article class="social_links">
+                <a href="https://www.linkedin.com/in/etienne-z/">
+                    <span style="font-size: 48px; color: Dodgerblue;">
+                        <i class="fab fa-linkedin-in"></i>
+                    </span>
+                </a>
+                <a href="https://github.com/Ethan081">
+                    <span style="font-size: 48px; color: Dodgerblue;">
+                        <i class="fab fa-github"></i>
+                    </span>
+                </a>
+            </article>
+
+        </section>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
